771c57de5eea78e185051d86448cd17ffb97a7de
Toss2204 <izotov7428@gmail.com>
2025-01-10 22:11:52 +0300

    2 начала

--- CatalogOfPlanets.cs
+++ CatalogOfPlanets.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +11,24 @@ namespace DZ_Otus_7
     public class CatalogOfPlanets
     {
         public string Name;
+        
 
         public CatalogOfPlanets()
         {
-            
+            var Venus = new Planet("Venus", 2, 12100, planet: null);
+            var Earth = new Planet("Earth", 3, 40075, Venus);
+            var Mars = new Planet("Mars", 4, 21326, Earth);
+
+            List<Planet> planets = new List<Planet>() { Venus, Earth, Mars };
         }
-            
+
+
+        
+
+
         public string GetPlanet(string name) 
         {
-        return name;
+            return name;
         }
 
     }
---
+++ Planet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Otus_7
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public int NumFromSun { get; set; }
+        public int LengthEcuator { get; set; }
+        public Planet PreviousPlanet { get; set; }
+
+        public Planet(string name,int num, int length, Planet planet )
+        {
+            Name=name;
+            NumFromSun=num;
+            LengthEcuator=length;
+            PreviousPlanet=planet;
+        }
+
+    }
+}
